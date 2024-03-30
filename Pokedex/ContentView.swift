8b4dc676f965fb13ff737e5f074b3f82c35666c3
Victor Ulloa <>
2024-03-30 16:13:49 -0400

    project created

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Victor Ulloa on 2024-03-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Victor Ulloa on 2024-03-30.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
